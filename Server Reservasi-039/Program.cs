e10a3334c7b02c645393642d121be522cf808162
Yoga Paripurna <63484583+YogaParipurna@users.noreply.github.com>
2021-12-20 09:04:09 +0700

    Yoga Paripurna 20190140039
    Yoga Paripurna 20190140039

---
+++ Server Reservasi-039/Program.cs
@@ -0,0 +1,32 @@
+﻿using ServiceReservasi_039;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_Reservasi_039
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Service1));
+                hostObj.Open();
+                Console.WriteLine("Server is Readyyyyy!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
